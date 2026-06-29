a9a5cb0d1695c01024888bad69c82a06d0e22115
timos <timo.schulze@live.com>
2026-06-28 20:27:00 -0700

    Camera Vibrate fix 1

--- components/camera/vaultFeedback.ts
+++ components/camera/vaultFeedback.ts
@@ -23,29 +23,4 @@ export function playVaultClick() {
 
   oscillator.start();
   oscillator.stop(audioContext.currentTime + 0.08);
-}export function vibrate(pattern: number | number[] = 40) {
-  if (typeof navigator !== "undefined" && "vibrate" in navigator) {
-    navigator.vibrate(pattern);
-  }
-}
-
-export function playVaultClick() {
-  const audioContext = new AudioContext();
-  const oscillator = audioContext.createOscillator();
-  const gain = audioContext.createGain();
-
-  oscillator.type = "sine";
-  oscillator.frequency.setValueAtTime(180, audioContext.currentTime);
-
-  gain.gain.setValueAtTime(0.08, audioContext.currentTime);
-  gain.gain.exponentialRampToValueAtTime(
-    0.001,
-    audioContext.currentTime + 0.08
-  );
-
-  oscillator.connect(gain);
-  gain.connect(audioContext.destination);
-
-  oscillator.start();
-  oscillator.stop(audioContext.currentTime + 0.08);
 }
